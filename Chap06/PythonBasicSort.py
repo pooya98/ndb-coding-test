db78580e86d5d19c1b902c5e03f83eebb7b9d69b
pooya98 <pooya98@naver.com>
2022-06-18 00:22:06 +0900

    Chap06 (1)

---
+++ Chap06/PythonBasicSort.py
@@ -0,0 +1,18 @@
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+result = sorted(array)
+
+print(array)
+print(result)
+
+array.sort()
+print(array)
+
+array = [('바나나', 2), ('사과', 5), ('당근', 3)]
+
+def setting(data):
+    return data[1]
+
+result = sorted(array, key=setting)
+
+print(result)
