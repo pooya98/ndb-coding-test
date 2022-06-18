2517a9ff2b5c7933cf498281d470f7f9d51391d8
pooya98 <pooya98@naver.com>
2022-06-18 12:56:29 +0900

    Chap07 (1)

---
+++ Chap07/SequentialSearch.py
@@ -0,0 +1,15 @@
+def sequential_search(array, target):
+    for i in range(len(target)):
+        if array[i] == target:
+            return i + 1
+
+print("생성할 원소 개수를 입력한 다음 한칸 띄고 찾을 문자열을 입력하세요.")
+input_data = input().split()
+
+n = int(input_data[0])
+target = input_data[1]
+
+print("앞서 적은 원소 개수만큼 문자열을 입력하세요. 구분은 띄어쓰기 한 칸으로 합니다.")
+array = input().split()
+
+print(sequential_search(array, target))
